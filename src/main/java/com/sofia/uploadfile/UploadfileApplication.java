98bf456a5fb1b1fb1116d4717f87034ffaf9d35b
Ronny Villarroel <ronaldovm.villarroel@gmail.com>
2020-09-28 15:29:38 -0400

    Initial commit

---
+++ src/main/java/com/sofia/uploadfile/UploadfileApplication.java
@@ -0,0 +1,27 @@
+package com.sofia.uploadfile;
+
+import com.sofia.uploadfile.storage.StorageProperties;
+import com.sofia.uploadfile.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class UploadfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadfileApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
+
+}
---
+++ src/main/java/com/sofia/uploadfile/storage/StorageException.java
@@ -0,0 +1,12 @@
+package com.sofia.uploadfile.storage;
+
+public class StorageException extends RuntimeException {
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
